c70c8d38c487fb6424b8b95cd61bdf072176f1da
Baird-weng <644672334@qq.com>
2016-10-18 17:27:12 +0800

    增加了主页的新闻列表，修改了导航栏样式。

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,21 +6,23 @@ Page({
     motto: 'Hello World',
     userInfo: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad: function (){
+    let that = this
+    wx.request({
+      url: 'http://news-at.zhihu.com/api/4/news/latest',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success (res) {
+
+        console.log(res)
+
+         that.setData({
+           banner: res.data.top_stories,
+           list: res.data.stories
+         })
+      }
     })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+    this.index = 1
   }
 })
